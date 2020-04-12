7613064dab9be5818bee7160921e9ad036345864
Tomsathit <sathit.b5800315@gmail.com>
2020-04-12 17:39:11 +0700

    refactor Aes 128 code

--- aes-128-cbctest.py
+++ aes-128-cbctest.py
@@ -3,22 +3,18 @@ from Crypto.Cipher import AES
 import Crypto.Cipher.AES
 from binascii import hexlify, unhexlify
 
-print(len('2b7e151628aed2a6abf7158809cf4f3c'))
-key = unhexlify('2b7e151628aed2a6abf7158809cf4f3c')    #32 byte
-IV = unhexlify('000102030405060708090a0b0c0d0e0f')     #32 byte
+key = unhexlify('2b7e151628aed2a6abf7158809cf4f3c')    #16 byte
+IV = unhexlify('000102030405060708090a0b0c0d0e0f')     #16 byte
 
-s = '0000000000sathit'.encode('utf-8')
-print(type(s), s)
-s = s.hex()
-plaintext1 = unhexlify(s)
-
-print(plaintext1)
+plaintextbegin = '0000000000sathit'.encode('utf-8') 
+print(" plaintextbegin encode utf-8 --> ",type(plaintextbegin), plaintextbegin)
 
 cipher = AES.new(key,AES.MODE_CBC,IV)
-ciphertext = cipher.encrypt(plaintext1)
-print(hexlify(ciphertext))
+ciphertext = cipher.encrypt(plaintextbegin)
+print("hexlify ciphertext--> ", hexlify(ciphertext))
 
 decipher = AES.new(key,AES.MODE_CBC,IV)
 plaintext = decipher.decrypt(ciphertext)
-print(plaintext == plaintext1)                        # test if decryption was successful
-print(plaintext.decode('utf-8'))
+print(plaintext)
+textstr = plaintext.decode('utf-8')
+print(type(textstr), textstr)
